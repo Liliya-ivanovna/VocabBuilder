f06f5240d32a461cd30265a4343fcfbb2db9aa0e
Liliya-ivanovna <lilyasviatenko@ukr.net>
2024-01-25 10:29:43 +0200

    Update Register.jsx

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,7 +3,9 @@ import icons from '../../assets/sprite.svg';
 
 const Register=()=>{
     return (<>
-   <svg width="40px" height="40px"> <use href={icons + '#icon-Craftwork'}/></svg>
+   <div><svg width="40px" height="40px"> <use href={icons + '#icon-Craftwork'}/></svg>
+   <h3>VocabBuilder</h3></div>
+   <h1>Register</h1>
     <RegisterForm/>
     </>)
 }
